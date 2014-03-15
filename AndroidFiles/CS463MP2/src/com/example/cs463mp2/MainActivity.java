e5a32376924ab1feb013f7e79251e97be64f175d
Will Ratner <wratner2@illinois.edu>
2014-03-15 13:29:57 -0500

    Updated Android Files
    getUidPackets still not working correctly

--- AndroidFiles/CS463MP2/src/com/example/cs463mp2/MainActivity.java
+++ AndroidFiles/CS463MP2/src/com/example/cs463mp2/MainActivity.java
@@ -1,73 +1,131 @@
 package com.example.cs463mp2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private MonitorService s;
+	/*
+	 * private Handler mHandler = new Handler(); private long mStartRX = 0;
+	 * private long mStartTX = 0;
+	 */
+	private int UID;
+	public SimpleDateFormat dateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");
+	public String currentTimeStamp = null;
 	
-	private Handler mHandler = new Handler();
-	private long mStartRX = 0;
-	private long mStartTX = 0;
-    private int UID;
-
+	public int i = 0;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //int UID = 0;
-        final Button startButton = (Button) findViewById(R.id.startButton);
-        final Button stopButton = (Button) findViewById(R.id.stopButton);
-        final PackageManager pm = getPackageManager();
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        for (ApplicationInfo packageInfo : packages) {
-        	if(packageInfo.packageName.equals("com.webmd.android")) {
-        		this.UID = packageInfo.uid;
-        	}
-        	Log.i("Check UID", "UID is: " + UID);
-        	
-        }
-        startButton.setOnClickListener(new View.OnClickListener() {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// int UID = 0;
+		//final Button startButton = (Button) findViewById(R.id.startButton);
+		final Button stopButton = (Button) findViewById(R.id.stopButton);
+		final PackageManager pm = getPackageManager();
+		List<ApplicationInfo> packages = pm
+				.getInstalledApplications(PackageManager.GET_META_DATA);
+		for (ApplicationInfo packageInfo : packages) {
+			if (packageInfo.packageName.equals("com.webmd.android")) {
+				this.UID = packageInfo.uid;
+			}
+			Log.i("Check UID", "UID is: " + UID);
+
+		}
+		/*startButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO start monitoring traffic
-				Toast.makeText(getApplicationContext(), Integer.toString(UID), Toast.LENGTH_LONG).show();
-				Intent i = new Intent(MainActivity.this, TrafficService.class);
-				i.putExtra(TrafficService.UID, Integer.toString(UID));
-				i.putExtra(TrafficService.ACTION, "start");
-				startService(i);
+				Toast.makeText(getApplicationContext(), Integer.toString(UID),
+						Toast.LENGTH_LONG).show();
+				
 				
 			}
-		});
-        stopButton.setOnClickListener(new View.OnClickListener() {
+		});*/
+		stopButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO stop monitoring traffic and save file
-				Intent i = new Intent(getApplicationContext(), TrafficService.class);
-				i.putExtra(TrafficService.UID, Integer.toString(UID));
-				i.putExtra(TrafficService.ACTION, "stop");
-				
+				s.setStop();
+				/*currentTimeStamp = dateFormat.format(new Date());
+				File root = android.os.Environment
+						.getExternalStorageDirectory();
+				File dir = new File(root.getAbsolutePath() + "/webMDTraffic");
+				dir.mkdirs();
+				File file = new File(dir, "test" + String.valueOf(i) + ".txt");
+				if (file.exists()) {
+					file.delete();
+				}
+				try {
+					FileOutputStream f = new FileOutputStream(file);
+					PrintWriter pw = new PrintWriter(f);
+					pw.println(currentTimeStamp + "; "
+							+ String.valueOf(s.getStartRxBytes()) + "; "
+							+ String.valueOf(s.getStartTxBytes()) + "; "
+							+ String.valueOf(-1) + "; "
+							+ String.valueOf(-1));
+					pw.flush();
+					pw.close();
+					f.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+					Log.e("Error", "File Not found", e);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					Log.e("Error", "IO ERROR", e);
+				}
+				i++;*/
 			}
 		});
-    }
+	}
+	public void startMonitoring(View view) {
+		Intent i = new Intent(getBaseContext(), MonitorService.class);
+		getBaseContext().startService(i);
+	}
+	protected void onResume() {
+		super.onResume();
+		Intent intent = new Intent(this, MonitorService.class);
+		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+	}
 
+	protected void onPause() {
+		super.onPause();
+		unbindService(mConnection);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder binder) {
+			MonitorService.MyBinder b = (MonitorService.MyBinder) binder;
+			s = b.getService();
+			Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT)
+					.show();
+			
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			s = null;
+		}
+	};
+
 }
--- AndroidFiles/CS463MP2/src/com/example/cs463mp2/TrafficService.java
+++ AndroidFiles/CS463MP2/src/com/example/cs463mp2/TrafficService.java
@@ -1,26 +1,18 @@
 package com.example.cs463mp2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.net.TrafficStats;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
 
-public class TrafficService extends IntentService {
+public class TrafficService extends Service {
 
-	// private int result = Activity.RESULT_CANCELED;
-	public static final String UID = "uid";
-	public static final String ACTION = "action";
 	public long StartRxBytes = 0;
 	public long tempRxBytes = 0;
 	public long StartTxBytes = 0;
@@ -33,20 +25,17 @@ public class TrafficService extends IntentService {
 	public SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss");
 	public String currentTimeStamp = null;
-	
-	public TrafficService() {
-		super("TrafficService");
-	}
+	Handler handler;
+	public String uid = "10070";
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		String uid = intent.getStringExtra(UID);
-		String action = intent.getStringExtra(ACTION);
-		Log.i("Check if service started", "Service has Started");
-		Log.i("Service Action", "Action is: " + action);
-		if (action.equals(action)) {
-			while (1 == 1) {
+	public void onStart(Intent intent, int startId) {
+		// super.onStart(intent, startId);
+		Toast.makeText(this, "Service running", Toast.LENGTH_SHORT).show();
+		handler = new Handler() {
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+				// Toast.makeText(TrafficService.this, "5 secs has passed",
+				// Toast.LENGTH_SHORT).show();
 				StartRxBytes = TrafficStats
 						.getUidRxBytes(Integer.parseInt(uid));
 				if (StartRxBytes != tempRxBytes) {
@@ -62,53 +51,88 @@ public class TrafficService extends IntentService {
 					Log.i("TxBytes Change",
 							"StartTxBytes is : " + String.valueOf(StartTxBytes));
 				}
-				StartRxPackets = TrafficStats.getUidRxPackets(Integer
+				/*StartRxPackets = TrafficStats.getUidRxPackets(Integer
 						.parseInt(uid));
 				StartTxPackets = TrafficStats.getUidTxPackets(Integer
-						.parseInt(uid));
-
-				// }
-				Log.i("Test", "Skipped start action process");
-				// if (action == "stop")
-				// {
-				currentTimeStamp = dateFormat.format(new Date());
-				// generateFile("test.txt");
-				// }
-				File root = android.os.Environment
-						.getExternalStorageDirectory();
-				File dir = new File(root.getAbsolutePath() + "/webMDTraffic");
-				dir.mkdirs();
-				File file = new File(dir, "test" + String.valueOf(i) + ".txt");
-				if (file.exists()) {
-					file.delete();
-				}
-				try {
-					FileOutputStream f = new FileOutputStream(file);
-					PrintWriter pw = new PrintWriter(f);
-					pw.println(currentTimeStamp + "; "
-							+ String.valueOf(StartRxBytes) + "; "
-							+ String.valueOf(StartTxBytes) + "; "
-							+ String.valueOf(StartRxPackets) + "; "
-							+ String.valueOf(StartTxPackets));
-					pw.flush();
-					pw.close();
-					f.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					Log.e("Error", "File Not found", e);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					Log.e("Error", "IO ERROR", e);
-				}
-				i++;
+						.parseInt(uid));*/
 			}
-		}
+		};
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while (true) {
+					try {
+						Thread.sleep(5000);
+						handler.sendEmptyMessage(0);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	/*
+	 * // private int result = Activity.RESULT_CANCELED; public static final
+	 * String UID = "uid"; public static final String ACTION = "action"; public
+	 * long StartRxBytes = 0; public long tempRxBytes = 0; public long
+	 * StartTxBytes = 0; public long tempTxBytes = 0; public long StartRxPackets
+	 * = 0; public long tempRxPackets = 0; public long StartTxPackets = 0;
+	 * public long tempTxPackets = 0; public int i = 0; public SimpleDateFormat
+	 * dateFormat = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss"); public String
+	 * currentTimeStamp = null;
+	 * 
+	 * public TrafficService() { super("TrafficService"); }
+	 * 
+	 * @SuppressLint("NewApi")
+	 * 
+	 * @Override protected void onHandleIntent(Intent intent) { String uid =
+	 * intent.getStringExtra(UID); String action =
+	 * intent.getStringExtra(ACTION); Log.i("Check if service started",
+	 * "Service has Started"); Log.i("Service Action", "Action is: " + action);
+	 * if (action.equals("start")) { //while (1 == 1) { StartRxBytes =
+	 * TrafficStats .getUidRxBytes(Integer.parseInt(uid)); if (StartRxBytes !=
+	 * tempRxBytes) { Log.i("Monitor Traffic",
+	 * "Starting to monitor traffic now"); tempRxBytes = StartRxBytes;
+	 * Log.i("RxBytes Change", "StartRxBytes is: " +
+	 * String.valueOf(StartRxBytes)); } StartTxBytes = TrafficStats
+	 * .getUidTxBytes(Integer.parseInt(uid)); if (StartTxBytes != tempTxBytes) {
+	 * tempTxBytes = StartTxBytes; Log.i("TxBytes Change", "StartTxBytes is : "
+	 * + String.valueOf(StartTxBytes)); } StartRxPackets =
+	 * TrafficStats.getUidRxPackets(Integer .parseInt(uid)); StartTxPackets =
+	 * TrafficStats.getUidTxPackets(Integer .parseInt(uid));
+	 * 
+	 * // } Log.i("Test", "Skipped start action process"); //} } if
+	 * (action.equals("stop")) {
+	 * 
+	 * currentTimeStamp = dateFormat.format(new Date()); //
+	 * generateFile("test.txt");
+	 * 
+	 * File root = android.os.Environment.getExternalStorageDirectory(); File
+	 * dir = new File(root.getAbsolutePath() + "/webMDTraffic"); dir.mkdirs();
+	 * File file = new File(dir, "test" + String.valueOf(i) + ".txt"); if
+	 * (file.exists()) { file.delete(); } try { FileOutputStream f = new
+	 * FileOutputStream(file); PrintWriter pw = new PrintWriter(f);
+	 * pw.println(currentTimeStamp + "; " + String.valueOf(StartRxBytes) + "; "
+	 * + String.valueOf(StartTxBytes) + "; " + String.valueOf(StartRxPackets) +
+	 * "; " + String.valueOf(StartTxPackets)); pw.flush(); pw.close();
+	 * f.close(); } catch (FileNotFoundException e) { e.printStackTrace();
+	 * Log.e("Error", "File Not found", e); } catch (IOException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); Log.e("Error",
+	 * "IO ERROR", e); } //i++;
+	 * 
+	 * } }
+	 * 
+	 * @Override public IBinder onBind(Intent intent) { return null; }
+	 */
 
 }
